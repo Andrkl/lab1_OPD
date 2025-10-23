ced5a49a8ea6d081a78634b30e5540a19ae1044b
Andrkl <155077996+Andrkl@users.noreply.github.com>
2025-10-23 14:55:13 +0300

    Update Script3.sh

--- Script3.sh
+++ Script3.sh
@@ -4,7 +4,7 @@ cp raichu6 munna6/masquerain
 ln -s munna6 Copy_43
 chmod u+w mandibuzz0
 ln nidorina0 mandibuzz0/medititenidorina
-chmod u+x masquerain4
+chmod u+w masquerain4
 cp masquerain4 mandibuzz0/medititemasquerain
 cat hitmontop5/geodude munna6/golduck > nidorina0_49
 ln -s masquerain4 mandibuzz0/musharnamasquerain
