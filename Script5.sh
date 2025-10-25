bae788f9926caeddb6d693353c135a3ab88d6629
Andrkl <155077996+Andrkl@users.noreply.github.com>
2025-10-25 15:58:38 +0300

    Update Script5.sh

--- Script5.sh
+++ Script5.sh
@@ -1,4 +1,5 @@
 cd lab0
+chmod u+w mandibuzz0/medititenidorina munna6/masquerain/hitmontop5/igglybuff munna6/masquerain/hitmontop5/infernape munna6/masquerain/hitmontop5/dewott munna6/beedrill
 rm masquerain4
 rm hitmontop5/geodude
 rm mandibuzz0/musharnamasquerain
