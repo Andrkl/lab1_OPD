b6ca6729a6f9a42ed59defa2276903725e3e3891
Andrkl <155077996+Andrkl@users.noreply.github.com>
2025-10-25 16:25:40 +0300

    Update Chmodback.sh

--- Chmodback.sh
+++ Chmodback.sh
@@ -1,7 +1,4 @@
-chmod u-w lab0/munna6/masquerain
 chmod u-w lab0/mandibuzz0
-chmod u-rw lab0/masquerain4
 chmod u-w lab0/munna6/masquerain
 chmod u-rw lab0/mandibuzz0/watchog
 chmod u-rw lab0/mandibuzz0/typhlosion
-chmod u-w lab0/mandibuzz0/medititenidorina lab0/munna6/masquerain/hitmontop5/igglybuff lab0/munna6/masquerain/hitmontop5/infernape lab0/munna6/masquerain/hitmontop5/dewott lab0/munna6/beedrill
